2760a05492ba8e980a3087f4495d1eb6fd50c955
ericmesmo <oliveiraericm@gmail.com>
2023-04-07 10:18:50 -0300

    add

---
+++ packages/docs/.storybook/preview.js
@@ -0,0 +1,35 @@
+import { themes } from '@storybook/theming'
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  docs: {
+    theme: themes.dark
+  },
+  /* a11y: {
+    config: {
+      rules: [
+        {
+          // This tells Axe to run the 'autocomplete-valid' rule on selectors
+          // that match '*:not([autocomplete="nope"])' (all elements except [autocomplete="nope"]).
+          // This is the safest way of ignoring a violation across all stories,
+          // as Axe will only ignore very specific elements and keep reporting
+          // violations on other elements of this rule.
+          id: 'autocomplete-valid',
+          selector: '*:not([autocomplete="nope"])',
+        },
+        {
+          // To disable a rule across all stories, set `enabled` to `false`.
+          // Use with caution: all violations of this rule will be ignored!
+          id: 'autocomplete-valid',
+          enabled: false,
+        },
+      ],
+    },
+  }, */
+}
